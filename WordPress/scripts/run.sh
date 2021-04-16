dca86bd9b4e5b8a5230c399bb300034fa602f8b1
Guilherme Rodrigues <guilherme.araujo@grancursosonline.com.br>
2021-04-16 11:20:50 -0300

    fix: caminho dos scripts

--- WordPress/scripts/run.sh
+++ WordPress/scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sh scripts/install_dependencies.sh
-sh scripts/change_permissions.sh
-sh scripts/start_server.sh
-sh scripts/create_test_db.sh
-sh scripts/stop_server.sh
+sh install_dependencies.sh
+sh change_permissions.sh
+sh start_server.sh
+sh create_test_db.sh
+sh stop_server.sh
